738901dadff3e64d8db3bbb05727f5199dd24c8d
Jonathan Olson <jonathan.olson@colorado.edu>
2018-03-23 20:51:55 -0600

    Adding bug note about https://github.com/phetsims/scenery/issues/750

--- js/building/view/ShapeContainerNode.js
+++ js/building/view/ShapeContainerNode.js
@@ -66,7 +66,7 @@ define( function( require ) {
           // TODO: can we share this code with ShapeStackNode? One is stroked and has a separate color, etc.
           for ( var i = 0; i < denominator; i++ ) {
             var angle = -i * 2 * Math.PI / denominator;
-            // TODO: remove commented out code once figured out (Chrome bug?)
+            // TODO: remove commented out code once figured out (Chrome bug?) See https://github.com/phetsims/scenery/issues/750
             // separatorShape.moveTo( 0, 0 ).lineTo( CIRCLE_RADIUS * Math.cos( angle ), CIRCLE_RADIUS * Math.sin( angle ) );
             separatorShape.moveTo( 0.01 * Math.cos( angle ), 0.01 * Math.sin( angle ) ).lineTo( CIRCLE_RADIUS * Math.cos( angle ), CIRCLE_RADIUS * Math.sin( angle ) );
           }
