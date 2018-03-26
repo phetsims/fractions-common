c0d980e44eeba6ffc58be7a5b60f392f44ab6628
Jonathan Olson <jonathan.olson@colorado.edu>
2018-03-26 14:27:39 -0600

    Extracting LabShapePanel

--- js/building/view/BuildingLabScreenView.js
+++ js/building/view/BuildingLabScreenView.js
@@ -10,32 +10,20 @@ define( function( require ) {
 
   // modules
   var AlignBox = require( 'SCENERY/nodes/AlignBox' );
-  var AlignGroup = require( 'SCENERY/nodes/AlignGroup' );
   var arrayRemove = require( 'PHET_CORE/arrayRemove' );
-  var Bounds2 = require( 'DOT/Bounds2' );
   var DerivedProperty = require( 'AXON/DerivedProperty' );
-  var DragListener = require( 'SCENERY/listeners/DragListener' );
-  var Fraction = require( 'PHETCOMMON/model/Fraction' );
   var fractionsCommon = require( 'FRACTIONS_COMMON/fractionsCommon' );
-  var FractionsCommonColorProfile = require( 'FRACTIONS_COMMON/common/view/FractionsCommonColorProfile' );
   var FractionsCommonConstants = require( 'FRACTIONS_COMMON/common/FractionsCommonConstants' );
-  var HBox = require( 'SCENERY/nodes/HBox' );
   var inherit = require( 'PHET_CORE/inherit' );
+  var LabShapePanel = require( 'FRACTIONS_COMMON/building/view/LabShapePanel' );
   var Matrix3 = require( 'DOT/Matrix3' );
   var ModelViewTransform2 = require( 'PHETCOMMON/view/ModelViewTransform2' );
-  var MutableOptionsNode = require( 'SUN/MutableOptionsNode' );
   var Node = require( 'SCENERY/nodes/Node' );
-  var Panel = require( 'SUN/Panel' );
-  var Property = require( 'AXON/Property' );
-  var RadioButtonGroup = require( 'SUN/buttons/RadioButtonGroup' );
-  var Representation = require( 'FRACTIONS_COMMON/common/enum/Representation' );
   var ResetAllButton = require( 'SCENERY_PHET/buttons/ResetAllButton' );
   var ScreenView = require( 'JOIST/ScreenView' );
-  var ShapeGroup = require( 'FRACTIONS_COMMON/building/model/ShapeGroup' );
   var ShapeGroupNode = require( 'FRACTIONS_COMMON/building/view/ShapeGroupNode' );
   var ShapePiece = require( 'FRACTIONS_COMMON/building/model/ShapePiece' );
   var ShapePieceNode = require( 'FRACTIONS_COMMON/building/view/ShapePieceNode' );
-  var ShapeStackNode = require( 'FRACTIONS_COMMON/building/view/ShapeStackNode' );
 
   // constants
   var PANEL_MARGIN = FractionsCommonConstants.PANEL_MARGIN;
@@ -57,118 +45,30 @@ define( function( require ) {
     // @public {ModelViewTransform2}
     this.modelViewTransform = new ModelViewTransform2( Matrix3.translationFromVector( this.layoutBounds.center ) );
 
-    // TODO: Move all this code out to a named panel?
-    var representationSelectionNode = new MutableOptionsNode( RadioButtonGroup, [ model.topRepresentationProperty, [
-      {
-        value: Representation.CIRCLE,
-        node: new ShapePieceNode( new ShapePiece( new Fraction( 1, 1 ), Representation.CIRCLE, FractionsCommonColorProfile.labCircleFillProperty ), {
-          scale: 0.3
-        } )
-      },
-      {
-        value: Representation.VERTICAL_BAR,
-        node: new ShapePieceNode( new ShapePiece( new Fraction( 1, 1 ), Representation.VERTICAL_BAR, FractionsCommonColorProfile.labBarFillProperty ), {
-          scale: 0.3
-        } )
-      }
-    ] ], _.extend( {
-      orientation: 'vertical',
-      buttonContentXMargin: 6,
-      buttonContentYMargin: 6,
-      selectedLineWidth: 2,
-      touchAreaXDilation: 5,
-      touchAreaYDilation: 2.5,
-      spacing: 5
-    } ), {
-      selectedStroke: FractionsCommonColorProfile.radioStrokeProperty,
-      baseColor: FractionsCommonColorProfile.radioBaseProperty
-    } );
-
-    var stackAlignGroup = new AlignGroup();
-    function createStackNode( stack ) {
-      var node = new ShapeStackNode( stack, {
-        pickable: false
-      } );
-      return new AlignBox( node, {
-        group: stackAlignGroup,
-        cursor: 'pointer',
-        inputListeners: [
-          DragListener.createForwardingListener( function( event ) {
-            var shapePiece = new ShapePiece( stack.fraction, stack.representation, stack.colorProperty );
-            shapePiece.positionProperty.value = self.modelViewTransform.viewToModelPosition( self.globalToLocalPoint( event.pointer.point ) );
-            model.activeShapePieces.push( shapePiece );
-            // TODO: factor this "find" usage out
-            var shapePieceNode = _.find( self.shapePieceNodes, function( shapePieceNode ) {
-              return shapePieceNode.shapePiece === shapePiece;
-            } );
-            shapePieceNode.dragListener.press( event, shapePieceNode );
-          } )
-        ]
-      } );
-    }
-    var circleStackNodes = model.circleStacks.map( createStackNode );
-    var barStackNodes = model.barStacks.map( createStackNode );
-
-    function createGroupIcon( representation ) {
-      var iconGroup = new ShapeGroup( representation );
-      var iconNode = new ShapeGroupNode( iconGroup, {
-        isIcon: true,
-        scale: FractionsCommonConstants.SHAPE_BUILD_SCALE,
-        pickable: false
-      } );
-      // TODO: better way? At least this is safe
-      iconNode.localBounds = iconNode.localBounds.withMinY( iconNode.localBounds.minY - 2 * iconNode.localBounds.centerY );
-      return new AlignBox( iconNode, {
-        group: stackAlignGroup,
-        cursor: 'pointer',
-        inputListeners: [
-          DragListener.createForwardingListener( function( event ) {
-            // TODO: encapsulation
-            var shapeGroup = model.addShapeGroup( representation );
-            shapeGroup.positionProperty.value = self.modelViewTransform.viewToModelPosition( self.globalToLocalPoint( event.pointer.point ) );
-            var shapeGroupNode = _.find( self.shapeGroupNodes, function( shapeGroupNode ) {
-              return shapeGroupNode.shapeGroup === shapeGroup;
-            } );
-            shapeGroupNode.dragListener.press( event, shapeGroupNode );
-            event.handle(); // for our selection
-          } )
-        ]
-      } );
-    }
-    var circleGroupIcon = createGroupIcon( Representation.CIRCLE );
-    var barGroupIcon = createGroupIcon( Representation.VERTICAL_BAR );
-
-    var STACK_PADDING = 20;
-
-    // TODO: a better way of doing this. maybe make it an AlignBox feature
-    function linkPointerAreas( node ) {
-      Property.multilink( [ stackAlignGroup.maxWidthProperty, stackAlignGroup.maxHeightProperty ], function( width, height ) {
-        var bounds = new Bounds2( -STACK_PADDING / 2, 0, width + STACK_PADDING / 2, height );
-        node.mouseArea = bounds;
-        node.touchArea = bounds;
-      } );
-    }
-    circleStackNodes.forEach( linkPointerAreas );
-    barStackNodes.forEach( linkPointerAreas );
-    linkPointerAreas( circleGroupIcon );
-    linkPointerAreas( barGroupIcon );
-
-    var shapeBox = new HBox( {
-      spacing: STACK_PADDING
-    } );
-    model.topRepresentationProperty.link( function( representation ) {
-      var leftSideNodes = [ representationSelectionNode ];
-      var middleNodes = representation === Representation.CIRCLE ? circleStackNodes : barStackNodes;
-      var rightSideNodes = [ representation === Representation.CIRCLE ? circleGroupIcon : barGroupIcon ];
-      shapeBox.children = leftSideNodes.concat( middleNodes ).concat( rightSideNodes );
-    } );
-
     // TODO: background color customizable
-    // @private {Panel}
-    this.shapePanel = new Panel( shapeBox, {
-      xMargin: 15
+    // @private {Node}
+    this.shapePanel = new LabShapePanel( model, {
+      dragPieceFromStackListener: function( event, stack ) {
+        var shapePiece = new ShapePiece( stack.fraction, stack.representation, stack.colorProperty );
+        shapePiece.positionProperty.value = self.modelViewTransform.viewToModelPosition( self.globalToLocalPoint( event.pointer.point ) );
+        model.activeShapePieces.push( shapePiece );
+        // TODO: factor this "find" usage out
+        var shapePieceNode = _.find( self.shapePieceNodes, function( shapePieceNode ) {
+          return shapePieceNode.shapePiece === shapePiece;
+        } );
+        shapePieceNode.dragListener.press( event, shapePieceNode );
+      },
+      dragGroupFromStackListener: function( event, representation ) {
+        // TODO: encapsulation
+        var shapeGroup = model.addShapeGroup( representation );
+        shapeGroup.positionProperty.value = self.modelViewTransform.viewToModelPosition( self.globalToLocalPoint( event.pointer.point ) );
+        var shapeGroupNode = _.find( self.shapeGroupNodes, function( shapeGroupNode ) {
+          return shapeGroupNode.shapeGroup === shapeGroup;
+        } );
+        shapeGroupNode.dragListener.press( event, shapeGroupNode );
+        event.handle(); // for our selection
+      }
     } );
-    this.shapePanel.centerTop = this.layoutBounds.centerTop.plusXY( 0, PANEL_MARGIN );
 
     // @private {Node}
     this.groupLayer = new Node();
---
+++ js/building/view/LabShapePanel.js
@@ -0,0 +1,149 @@
+// Copyright 2017, University of Colorado Boulder
+
+/**
+ * TODO: doc
+ *
+ * @author Jonathan Olson <jonathan.olson@colorado.edu>
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var AlignBox = require( 'SCENERY/nodes/AlignBox' );
+  var AlignGroup = require( 'SCENERY/nodes/AlignGroup' );
+  var Bounds2 = require( 'DOT/Bounds2' );
+  var DragListener = require( 'SCENERY/listeners/DragListener' );
+  var Fraction = require( 'PHETCOMMON/model/Fraction' );
+  var fractionsCommon = require( 'FRACTIONS_COMMON/fractionsCommon' );
+  var FractionsCommonColorProfile = require( 'FRACTIONS_COMMON/common/view/FractionsCommonColorProfile' );
+  var FractionsCommonConstants = require( 'FRACTIONS_COMMON/common/FractionsCommonConstants' );
+  var HBox = require( 'SCENERY/nodes/HBox' );
+  var inherit = require( 'PHET_CORE/inherit' );
+  var MutableOptionsNode = require( 'SUN/MutableOptionsNode' );
+  var Panel = require( 'SUN/Panel' );
+  var Property = require( 'AXON/Property' );
+  var RadioButtonGroup = require( 'SUN/buttons/RadioButtonGroup' );
+  var Representation = require( 'FRACTIONS_COMMON/common/enum/Representation' );
+  var ShapeGroup = require( 'FRACTIONS_COMMON/building/model/ShapeGroup' );
+  var ShapeGroupNode = require( 'FRACTIONS_COMMON/building/view/ShapeGroupNode' );
+  var ShapePiece = require( 'FRACTIONS_COMMON/building/model/ShapePiece' );
+  var ShapePieceNode = require( 'FRACTIONS_COMMON/building/view/ShapePieceNode' );
+  var ShapeStackNode = require( 'FRACTIONS_COMMON/building/view/ShapeStackNode' );
+
+  /**
+   * @constructor
+   * @extends {Panel}
+   *
+   * @param {BuildingLabModel} model
+   * @param {Object} options
+   */
+  function LabShapePanel( model, options ) {
+
+    options = _.extend( {
+      dragPieceFromStackListener: null,
+      dragGroupFromStackListener: null
+    }, options );
+
+    // TODO: Move all this code out to a named panel?
+    var representationSelectionNode = new MutableOptionsNode( RadioButtonGroup, [ model.topRepresentationProperty, [
+      {
+        value: Representation.CIRCLE,
+        node: new ShapePieceNode( new ShapePiece( new Fraction( 1, 1 ), Representation.CIRCLE, FractionsCommonColorProfile.labCircleFillProperty ), {
+          scale: 0.3
+        } )
+      },
+      {
+        value: Representation.VERTICAL_BAR,
+        node: new ShapePieceNode( new ShapePiece( new Fraction( 1, 1 ), Representation.VERTICAL_BAR, FractionsCommonColorProfile.labBarFillProperty ), {
+          scale: 0.3
+        } )
+      }
+    ] ], _.extend( {
+      orientation: 'vertical',
+      buttonContentXMargin: 6,
+      buttonContentYMargin: 6,
+      selectedLineWidth: 2,
+      touchAreaXDilation: 5,
+      touchAreaYDilation: 2.5,
+      spacing: 5
+    } ), {
+      selectedStroke: FractionsCommonColorProfile.radioStrokeProperty,
+      baseColor: FractionsCommonColorProfile.radioBaseProperty
+    } );
+
+    var stackAlignGroup = new AlignGroup();
+    function createStackNode( stack ) {
+      var node = new ShapeStackNode( stack, {
+        pickable: false
+      } );
+      return new AlignBox( node, {
+        group: stackAlignGroup,
+        cursor: 'pointer',
+        inputListeners: [
+          DragListener.createForwardingListener( function( event ) {
+            // TODO: doc
+            options.dragPieceFromStackListener( event, stack );
+          } )
+        ]
+      } );
+    }
+    var circleStackNodes = model.circleStacks.map( createStackNode );
+    var barStackNodes = model.barStacks.map( createStackNode );
+
+    function createGroupIcon( representation ) {
+      var iconGroup = new ShapeGroup( representation );
+      var iconNode = new ShapeGroupNode( iconGroup, {
+        isIcon: true,
+        scale: FractionsCommonConstants.SHAPE_BUILD_SCALE,
+        pickable: false
+      } );
+      // TODO: better way? At least this is safe
+      iconNode.localBounds = iconNode.localBounds.withMinY( iconNode.localBounds.minY - 2 * iconNode.localBounds.centerY );
+      return new AlignBox( iconNode, {
+        group: stackAlignGroup,
+        cursor: 'pointer',
+        inputListeners: [
+          DragListener.createForwardingListener( function( event ) {
+            options.dragGroupFromStackListener( event, representation );
+          } )
+        ]
+      } );
+    }
+    var circleGroupIcon = createGroupIcon( Representation.CIRCLE );
+    var barGroupIcon = createGroupIcon( Representation.VERTICAL_BAR );
+
+    var STACK_PADDING = 20;
+
+    // TODO: a better way of doing this. maybe make it an AlignBox feature
+    function linkPointerAreas( node ) {
+      Property.multilink( [ stackAlignGroup.maxWidthProperty, stackAlignGroup.maxHeightProperty ], function( width, height ) {
+        var bounds = new Bounds2( -STACK_PADDING / 2, 0, width + STACK_PADDING / 2, height );
+        node.mouseArea = bounds;
+        node.touchArea = bounds;
+      } );
+    }
+    circleStackNodes.forEach( linkPointerAreas );
+    barStackNodes.forEach( linkPointerAreas );
+    linkPointerAreas( circleGroupIcon );
+    linkPointerAreas( barGroupIcon );
+
+    var shapeBox = new HBox( {
+      spacing: STACK_PADDING
+    } );
+    model.topRepresentationProperty.link( function( representation ) {
+      var leftSideNodes = [ representationSelectionNode ];
+      var middleNodes = representation === Representation.CIRCLE ? circleStackNodes : barStackNodes;
+      var rightSideNodes = [ representation === Representation.CIRCLE ? circleGroupIcon : barGroupIcon ];
+      shapeBox.children = leftSideNodes.concat( middleNodes ).concat( rightSideNodes );
+    } );
+
+    // TODO: background color customizable
+    Panel.call( this, shapeBox, {
+      xMargin: 15
+    } );
+  }
+
+  fractionsCommon.register( 'LabShapePanel', LabShapePanel );
+
+  return inherit( Panel, LabShapePanel );
+} );
