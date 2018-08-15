fd1155f81c3ce6a587f6fb7338d9821de6b00156
Jonathan Olson <jonathan.olson@colorado.edu>
2018-08-15 01:18:53 -0600

    Doc updates and level checks

--- js/game/model/FractionLevel.js
+++ js/game/model/FractionLevel.js
@@ -1,7 +1,7 @@
 // Copyright 2018, University of Colorado Boulder
 
 /**
- * TODO: doc
+ * Represents and handles generation of the levels for the "building" style fractions sims.
  *
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
@@ -24,7 +24,7 @@ define( require => {
   let generated3Shapes = false;
   let generated4Shapes = false;
 
-  // Convenience functions. TODO: What is the best way to have concise usage in the code?
+  // Convenience functions.
   const nextBoolean = () => phet.joist.random.nextBoolean();
   const sample = array => phet.joist.random.sample( array );
   const shuffle = array => phet.joist.random.shuffle( array );
@@ -106,7 +106,7 @@ define( require => {
       } ) );
     }
 
-    // TODO: note createCardsSameNumberEachType from Java
+    // note createCardsSameNumberEachType from Java
     static maxNumeratorUnitFractions( fractions ) {
       const maxNumerator = Math.max( ...fractions.map( f => f.numerator ) );
       return _.flatten( fractions.map( f => repeat( maxNumerator, new Fraction( 1, f.denominator ) ) ) );
@@ -134,6 +134,19 @@ define( require => {
     /**
      * Creates a challenge for (unmixed) shapes level 1.
      * @public
+     *
+     * Java doc:
+     * > Make two draws, one target should be from the set  {1/1, 2/2, 3/3} and the second draw for the next two targets
+     * > from the set {1/2, 1/3, 2/3}
+     *
+     * Design doc:
+     * > Two "draws", one target should be from the set  {1/1, 2/2, 3/3} and the second draw for the next two targets
+     * > from the set {1/2, 1/3, 2/3}
+     *
+     * We do three "draws", one from the first set, and two from the second set (if that's clear).
+     *
+     * NOTE: In the Java version, this randomly started as either pie/bar and STAYED for the entire sim duration.
+     * This is inconsistent with our general practices now, so now it always STARTS as pie, then is random.
      *
      * @param {number} levelNumber
      * @param {ColorDef} color
@@ -170,6 +183,19 @@ define( require => {
      * Creates a challenge for (unmixed) shapes level 2.
      * @public
      *
+     * Java doc:
+     * > Choose from a distribution of fractions ranging from 1/2 to 4/5. The numerator could be 1, 2, 3, or 4 and the
+     * > denominator could be 2, 3, 4, or 5 with the stipulation that the fraction is always less than 1. No "wholes"
+     * > will appear in the shapes pile.
+     *
+     * Design doc:
+     * > Choosing from a distribution of fractions ranging from 1/2 to 4/5.  The numerator can be 1, 2, 3, or 4 and the
+     * > denominator could be 2, 3, 4, or 5 with the stipulation that the fraction is always less than 1. No "wholes" in
+     * > the shapes piles. 2 possible ways to make at least one of the targets
+     *
+     * NOTE: In the Java version, this randomly started as either pie/bar and STAYED for the entire sim duration.
+     * This is inconsistent with our general practices now, so now it always STARTS as bar, then is random.
+     *
      * @param {number} levelNumber
      * @param {ColorDef} color
      * @returns {FractionChallenge}
@@ -204,6 +230,17 @@ define( require => {
      * Creates a challenge for (unmixed) shapes level 3.
      * @public
      *
+     * Java and design doc (identical for this level):
+     * > Like level 2, but now fractions ranging from 1/1 to 6/6, and with "whole" pieces available.
+     * > Number of pieces of each fraction allowing for multiple solutions
+     *
+     * NOTE: In the Java version, this randomly started as either pie/bar and STAYED for the entire sim duration.
+     * This is inconsistent with our general practices now, so now it always STARTS as pie, then is random.
+     *
+     * TODO: Descriptions do NOT note anything about the "max numerator" selection. It's also conceivable that three
+     * of the same numerator could be selected, thus NOT being like level2 (not two ways to make one) since it would
+     * have the exact number of needed pieces.
+     *
      * @param {number} levelNumber
      * @param {ColorDef} color
      * @returns {FractionChallenge}
@@ -225,6 +262,23 @@ define( require => {
     /**
      * Creates a challenge for (unmixed) shapes level 4.
      * @public
+     *
+     * Java doc:
+     * > Goal: build the same targets with constrained pieces.
+     * > 2 possible targets, which are {1/2, 1/1}.  For 1/1, constrain one of the targets so they must use two different
+     * > sizes.  For instance, only enough halves and quarters so they must do 1 half piece and 2 quarter pieces. Or 2
+     * > third pieces and 2 sixth pieces.
+     *
+     * Design doc:
+     * > All 3 targets the same, 2 possible target values {1/2, 1/1}.
+     * > No "whole" pieces available
+     * > constrain one of the targets so that two different sizes must be used.
+     *
+     * NOTE: In the Java version, this randomly started as either pie/bar and STAYED for the entire sim duration.
+     * This is inconsistent with our general practices now, so now it always STARTS as bar, then is random.
+     *
+     * TODO: The 3rd statement "constrain one of the targets so that two different sizes must be used" is false for the
+     * "half" version - Use 1/2 for one, 2/4 for another and 3/6 for the last.
      *
      * @param {number} levelNumber
      * @param {ColorDef} color
@@ -265,6 +319,22 @@ define( require => {
     /**
      * Creates a challenge for (unmixed) shapes level 5.
      * @public
+     *
+     * Java doc:
+     * > Pie shapes for this level
+     * > numerator able to range from 1-8, and denominator able to range from 1-8, with the number less than or equal to
+     * > 1
+     * > all pieces available to fulfill targets in the most straightforward way (so for instance if 3/8 appears there
+     * > will 3 1/8 pieces)
+     *
+     * Design doc:
+     * > - numerator able to range from 1-8, and denominator able to range from 1-8, with the number less than or equal
+     * >   to 1
+     * > - all pieces available to fulfill targets in the most straightforward way (so for instance if 3/8 appears there
+     * >   will 3 1/8 pieces)
+     *
+     * TODO: No note about the addition of the "extra" possibly-nonintuitive pieces in either doc. Should that be
+     * removed?
      *
      * @param {number} levelNumber
      * @param {ColorDef} color
