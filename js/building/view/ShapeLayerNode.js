e7286187abf979f9e4ec3c08c6101046fbd96be3
Jonathan Olson <jonathan.olson@colorado.edu>
2019-01-10 00:18:05 -0700

    Shape container layout fix, see https://github.com/phetsims/fractions-common/issues/67

--- js/building/view/ShapeLayerNode.js
+++ js/building/view/ShapeLayerNode.js
@@ -90,6 +90,10 @@ define( require => {
       else {
         throw new Error( `Unsupported representation for ShapeLayerNode: ${representation}` );
       }
+
+      // Use our current bounds as our permanent bounds (for layout), since no content should be able to be outside this
+      // (any pieces will be inside the container).
+      this.localBounds = this.localBounds;
     }
 
     /**
