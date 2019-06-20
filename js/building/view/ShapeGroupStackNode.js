cd3a85e1d8a343f59c20fed9e24f44f9dc876778
Jonathan Olson <jonathan.olson@colorado.edu>
2019-06-20 13:35:23 -0600

    Moving EnumerationMap to fractions-common

--- js/building/view/ShapeGroupStackNode.js
+++ js/building/view/ShapeGroupStackNode.js
@@ -11,7 +11,7 @@ define( require => {
   // modules
   const Bounds2 = require( 'DOT/Bounds2' );
   const BuildingRepresentation = require( 'FRACTIONS_COMMON/building/model/BuildingRepresentation' );
-  const EnumerationMap = require( 'FRACTIONS_COMMON/common/EnumerationMap' );
+  const EnumerationMap = require( 'PHET_CORE/EnumerationMap' );
   const fractionsCommon = require( 'FRACTIONS_COMMON/fractionsCommon' );
   const FractionsCommonConstants = require( 'FRACTIONS_COMMON/common/FractionsCommonConstants' );
   const Node = require( 'SCENERY/nodes/Node' );
