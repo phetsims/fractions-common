a2b8db2a6208d04964467a0f30435c5053d60147
Sam Reid <reids@colorado.edu>
2022-04-09 22:17:18 -0600

    Rename image files *.js => *.ts, see https://github.com/phetsims/chipper/issues/1218

--- js/fractionsCommonStrings.ts
+++ js/fractionsCommonStrings.ts
@@ -3,11 +3,23 @@
 /**
  * Auto-generated from modulify, DO NOT manually modify.
  */
-
+/* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
 import fractionsCommon from './fractionsCommon.js';
 
-const fractionsCommonStrings = getStringModule( 'FRACTIONS_COMMON' );
+type StringsType = {
+  'representationMax': string,
+  'mixedNumber': string,
+  'levelTitlePattern': string,
+  'myMatches': string,
+  'ok': string,
+  'timeNumberSec': string,
+  'mixedNumbersChooseYourLevel': string,
+  'fractionsChooseYourLevel': string,
+  'equation': string
+};
+
+const fractionsCommonStrings = getStringModule( 'FRACTIONS_COMMON' ) as StringsType;
 
 fractionsCommon.register( 'fractionsCommonStrings', fractionsCommonStrings );
 
