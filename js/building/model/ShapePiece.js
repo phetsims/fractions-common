98eaea8406daf998cd46284ef6ea7685737bd7db
phet-dev <phetadmin@gmail.com>
2022-05-24 03:36:35 -0600

    update copyright dates from daily grunt work

--- js/building/model/ShapePiece.js
+++ js/building/model/ShapePiece.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2020, University of Colorado Boulder
+// Copyright 2018-2022, University of Colorado Boulder
 
 /**
  * A movable shape "piece" that can be combined/placed into groups.
