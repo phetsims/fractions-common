0cc6c0e551f39475e50ba635bd665ad130067bd5
phet-dev <phetadmin@gmail.com>
2022-08-28 04:20:42 -0600

    update copyright dates from daily grunt work

--- js/intro/view/CellEntry.js
+++ js/intro/view/CellEntry.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2020, University of Colorado Boulder
+// Copyright 2018-2022, University of Colorado Boulder
 
 /**
  * Tracks a cell and it's corresponding view node, and handles visibility.
