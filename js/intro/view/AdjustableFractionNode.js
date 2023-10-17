91f698bb16576e784ca8e2e2bbdafb998cfbb3d3
phet-dev <phetadmin@gmail.com>
2023-10-17 03:16:11 -0600

    update copyright dates from daily grunt work

--- js/intro/view/AdjustableFractionNode.js
+++ js/intro/view/AdjustableFractionNode.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2022, University of Colorado Boulder
+// Copyright 2018-2023, University of Colorado Boulder
 
 /**
  * Displays a fraction with up/down spinners for both the numerator and denominator.
--- js/intro/view/RoundNumberSpinner.js
+++ js/intro/view/RoundNumberSpinner.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2022, University of Colorado Boulder
+// Copyright 2018-2023, University of Colorado Boulder
 
 /**
  * Displays an up/down control for incrementing/decrementing a Property
